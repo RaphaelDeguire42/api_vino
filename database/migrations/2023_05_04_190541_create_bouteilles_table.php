5f58174494f81509f1bda41451f527a0bfd781eb
RaphaelDeguire42 <e2295637@cmaisonneuve.qc.ca>
2023-05-05 10:01:44 -0400

    bouteilleHas type,format,pays et leurs migrations a chaques avec migration de bouteilles

--- database/migrations/2023_05_04_190541_create_bouteilles_table.php
+++ database/migrations/2023_05_04_190541_create_bouteilles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Extension\Table\Table;
 
 return new class extends Migration
 {
@@ -15,6 +16,19 @@ return new class extends Migration
     {
         Schema::create('bouteilles', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->string('code_saq');
+            $table->string('url_img');
+            $table->integer('garde');
+            $table->float('prix');
+            $table->integer('millesime');
+            $table->boolean('actif');
+            $table->unsignedBigInteger('id_type');
+            $table->foreign('id_type')->references('id')->on('types');
+            $table->unsignedBigInteger('id_pays');
+            $table->foreign('id_pays')->references('id')->on('pays');
+            $table->unsignedBigInteger('id_format');
+            $table->foreign('id_format')->references('id')->on('formats');
             $table->timestamps();
         });
     }
