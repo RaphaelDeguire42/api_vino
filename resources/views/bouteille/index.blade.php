7de2ed92a19265d0bd35ce6abc174387cfacb3a5
RaphaelDeguire42 <e2295637@cmaisonneuve.qc.ca>
2023-05-07 15:13:11 -0400

    changed styles places for material design

--- resources/views/bouteille/index.blade.php
+++ resources/views/bouteille/index.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <link rel="stylesheet" href="/components/styles.css">
+    <link rel="stylesheet" href="/css/material-styles.css">
     <link rel="stylesheet" href="/css/signalerErreur.css">
 </head>
 
