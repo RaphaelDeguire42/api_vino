7aa61c03a20d605321c0ab20c2f2b2a06eeb9a23
RaphaelDeguire42 <e2295637@cmaisonneuve.qc.ca>
2023-05-16 16:01:10 -0400

    routes pour formats types et pays

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BouteilleController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\FormatController;
+use App\Http\Controllers\PaysController;
 use App\Http\Controllers\CellierController;
 use App\Http\Controllers\AdminController;
 
@@ -25,6 +28,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['namespace' => 'App\Http\Controllers'], function(){
     Route::apiResource('bouteilles', BouteilleController::class); // CRUD pour Bouteilles
     Route::apiResource('celliers', CellierController::class); //CRUD pour Celliers
+    Route::apiResource('types', TypeController::class); //CRUD pour Types
+    Route::apiResource('pays', PaysController::class); //CRUD pour Pays
+    Route::apiResource('formats', FormatController::class); //CRUD pour Formats
 });
 
 // Route::group(['namespace' => 'App\Http\Controllers', 'middleware' => 'auth:sanctum'], function(){
