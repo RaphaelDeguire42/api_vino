f77f19ebf67adf45d29027b8c932bdbbb41caea1
RaphaelDeguire42 <e2295637@cmaisonneuve.qc.ca>
2023-05-16 21:43:38 -0400

    modification de la function store de cellier bouteille pour permettre de sauvegarder des bouteilles non lister

--- app/Http/Controllers/CellierBouteilleController.php
+++ app/Http/Controllers/CellierBouteilleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cellier_Bouteille;
+use App\Models\Bouteille;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -37,15 +38,29 @@ class CellierBouteilleController extends Controller
     public function store(Request $request)
     {
         $cellier_bouteille = new Cellier_Bouteille();
-        $cellier_bouteille->id_bouteille = $request['id_bouteille'];
+        $cellier_bouteille->id_bouteille = $request->has('id_bouteille') ? $request['id_bouteille'] : null;
         $cellier_bouteille->id_cellier = $request['id_cellier'];
         $cellier_bouteille->quantite = $request['quantite'];
         $cellier_bouteille->date_achat = Carbon::parse($request['date_achat'])->format('Y-m-d H:i:s');
         $cellier_bouteille->garde = $request['garde'];
         $cellier_bouteille->millesime = $request['millesime'];
+
+        if ($request->has('id_bouteille')) {
+            $bouteille = Bouteille::find($request['id_bouteille']);
+            if ($bouteille) {
+                // Fill missing data with bouteille data
+                $cellier_bouteille->nom = $bouteille->nom;
+                $cellier_bouteille->id_pays = $bouteille->id_pays;
+                $cellier_bouteille->url_img = $bouteille->url_img;
+            }
+        }
+
         $cellier_bouteille->save();
+
         return response()->json(['id' => $cellier_bouteille->id]);
     }
+
+
 
     /**
      * Display the specified resource.
