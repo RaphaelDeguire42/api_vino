a0951f1e52c3f997756edbb08daa9948a222b771
Francois Poulin <poulinfrancois77@gmail.com>
2023-05-23 09:38:15 -0400

    crud simple avant modif pour include

--- app/Http/Controllers/NoteCommentaireController.php
+++ app/Http/Controllers/NoteCommentaireController.php
@@ -14,9 +14,10 @@ class NoteCommentaireController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $note_Commentaires = Note_Commentaire::all();
 
+        return response()->json($note_Commentaires, 200);
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -24,9 +25,18 @@ class NoteCommentaireController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+   
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'id_bouteille' => 'required',
+            'note' => 'required|integer',
+            'commentaire' => 'required|string',
+        ]);
+
+        $note_Commentaire = Note_Commentaire::create($data);
+
+        return response()->json($note_Commentaire, 201);
     }
 
     /**
@@ -37,7 +47,7 @@ class NoteCommentaireController extends Controller
      */
     public function show(Note_Commentaire $note_Commentaire)
     {
-        //
+        return response()->json($note_Commentaire, 200);
     }
 
     /**
@@ -67,6 +77,8 @@ class NoteCommentaireController extends Controller
      */
     public function destroy(Note_Commentaire $note_Commentaire)
     {
-        //
+        $note_Commentaire->delete();
+
+        return response()->json(null, 204);
     }
 }
