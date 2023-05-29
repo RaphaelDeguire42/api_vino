6308e5d49fd9e33af179608b088c5f01d2cbef34
Francois Poulin <poulinfrancois77@gmail.com>
2023-05-29 12:05:52 -0400

    hotfix general AUTH

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,6 +42,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'id_role' => $request->id_role ?? 2,
         ]);
 
         return response()->json([
